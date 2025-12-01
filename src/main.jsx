dff007e9f45968f1fcaa6f9a6c6e0c5db83b88c6
kidselite <49541078+kidselite@users.noreply.github.com>
2025-11-30 19:04:22 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// 修正：嘗試使用相對路徑，假設 App.jsx 不在當前目錄，而是在上層目錄
-// 如果您的 App.jsx 和 index.html 放在同一層，而 src/main.jsx 在子目錄，這可能會解決問題。
-import App from '../App.jsx';
+// 修正：因為 App.jsx 和 main.jsx 都在 src/ 資料夾裡，所以必須使用相對路徑 './' (當前目錄)
+import App from './App.jsx';
 
 // 找到 index.html 裡的 <div id="root"> 並將 App 啟動
 ReactDOM.createRoot(document.getElementById('root')).render(
